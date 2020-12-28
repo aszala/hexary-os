600323b9e52cfa6c43b8d179c0060d2435592c61
aszala <zala.abhay@gmail.com>
2020-12-28 23:44:28 +0000

    Implemented clear function

--- src/kernel.c
+++ src/kernel.c
@@ -1,9 +1,24 @@
 #include "include/screen.h"
+#include "include/types.h"
+#include "include/keyboard.h"
 
 int main() {
 	clearScreen();
-	print("This is a test\n");
-	print("This is a test 2\n");
+	print("Welcome to HexaryOS\n");
+
+	uint8 run = 1;
+
+	while (run) {
+		print("\nHexaryOS>");
+
+		string cmd = readInput();
+
+		if (strEquals(cmd, "clear")) {
+			clearScreen();
+		} else {
+			print("\n" + cmd + ": command not found");
+		}
+	}
 
 	return 0;
 }
