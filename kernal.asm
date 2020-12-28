e321556dad5a71ab1c09216dd2c3a429a7e82f63
aszala <zala.abhay@gmail.com>
2020-12-28 07:33:00 +0000

    Fixed bugs

--- kernal.asm
+++ kernal.asm
@@ -1,5 +1,5 @@
 bits	32
-setion 	.text
+section 	.text
 	align	4
 	dd	0x1BADB002
 	dd	0x00
@@ -9,5 +9,5 @@ global start
 extern main	; Defined in external C file
 start:
 	cli
-	call maim
+	call main
 	hlt
--- kernal.c
+++ kernal.c
@@ -1,5 +1,7 @@
-main() {
+int main() {
 	char* vidmem = (char*)0xb8000;
 	vidmem[0] = 'H';
 	vidmem[1] = 0x04;
+
+	return 0;
 }
