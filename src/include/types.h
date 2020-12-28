ff7ef1df58955625b30150b2b2fdc8959c5a6b99
aszala <zala.abhay@gmail.com>
2020-12-28 09:49:53 +0000

    Moved C files into src

---
+++ src/include/types.h
@@ -0,0 +1,18 @@
+#ifndef TYPES_HEADER
+#define TYPES_HEADER
+
+typedef signed char int8;
+typedef unsigned char uint8;
+
+typedef signed short int16;
+typedef unsigned short unint16;
+
+typedef signed int int32;
+typedef unsigned int uint32;
+
+typedef signed long int64;
+typedef unsigned long uint64;
+
+typedef char* string;
+
+#endif
