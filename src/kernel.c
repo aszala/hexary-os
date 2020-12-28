ab0e1b015c563b25628e062f85407b2ef420416e
aszala <zala.abhay@gmail.com>
2020-12-28 21:46:34 +0000

    Removed test code

--- src/kernel.c
+++ src/kernel.c
@@ -1,14 +1,9 @@
 #include "include/screen.h"
-#include <stdio.h>
 
 int main() {
 	clearScreen();
 	print("This is a test\n");
 	print("This is a test 2\n");
-
-	char c = getchar();
-
-	printch(c);
 
 	return 0;
 }
