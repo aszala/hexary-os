fcc7929ad5282d77939863c3ba78d542fbd6c73e
aszala <zala.abhay@gmail.com>
2020-12-28 10:23:25 +0000

    Fixed types.h

--- src/include/types.h
+++ src/include/types.h
@@ -10,8 +10,8 @@ typedef unsigned short uint16;
 typedef signed int int32;
 typedef unsigned int uint32;
 
-typedef signed long int64;
-typedef unsigned long uint64;
+typedef signed long long int64;
+typedef unsigned long long uint64;
 
 typedef char* string;
 
